b23f1f0205d15d269fe45309a8a547a0f29ba828
belllee <bell.lizhongwei@gmail.com>
2020-01-12 22:14:18 +0800

    Create const.js

---
+++ const.js
@@ -0,0 +1,45 @@
+// import {NUMBER, STRING} from '../const.js'
+// computed: {
+//   NUMBER () {
+//     return NUMBER
+//   },
+//   STRING () {
+//     return STRING
+//   }
+// }
+
+export const NUMBER = {
+  _0: 0,
+  _1: 1,
+  _2: 2,
+  _3: 3,
+  _4: 4,
+  _5: 5,
+  _6: 6,
+  _7: 7,
+  _8: 8,
+  _9: 9,
+  _10: 10,
+  _20: 20,
+  _30: 30,
+  _40: 40,
+  _50: 50,
+  _60: 60,
+  _70: 70,
+  _80: 80,
+  _90: 90,
+  _100: 100,
+  _1000: 1000
+}
+
+export const STRING = {
+  empty: '',
+  success: 'success',
+  fail: 'fail',
+  error: 'error'
+}
+export const STRING_U = {
+  success: 'SUCCESS',
+  fail: 'FAIL',
+  error: 'ERROR'
+}
